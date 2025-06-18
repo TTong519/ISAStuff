c358fac4cff55cfdbe836b42ce2c8540a260f6bd
Tyler.Tong <Tyler.Tong@GMRDL3>
2025-06-18 10:59:34 -0700

    Add project files.

---
+++ Assembler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Assembler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Emulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Emulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
